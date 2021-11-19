453f0e74286e8ff197163ba2b8f8a6927ef01271
NARAYANREDDYDODLA <narayanreddydnr@gmail.com>
2021-11-19 00:05:50 -0600

    app

--- routes/costumes.js
+++ routes/costumes.js
@@ -1,5 +1,4 @@
 var express = require('express'); 
-const costume_controlers= require('../controllers/costume'); 
 const costume_controllers= require('../controllers/costume'); 
 const detail_controller =  require('../controllers/constumedetail');
 const createcostume_controller = require('../controllers/createcostume');
@@ -7,9 +6,9 @@ const costumeupdate_controller = require('../controllers/costumeupdate');
 var router = express.Router(); 
  
 /* GET costumes */ 
-router.get('/', costume_controlers.costume_view_all_Page ); 
-router.get('/costumes/:id', costume_controlers.costume_detail); 
-
+router.get('/', costume_controllers.costume_view_all_Page ); 
+router.get('/costumes/:id', costume_controllers.costume_detail);
+/* GET detail costume page */ 
 router.get('/detail', detail_controller.costume_view_one_Page); 
 /* GET create costume page */ 
 router.get('/create', createcostume_controller.costume_create_Page);
@@ -17,4 +16,4 @@ router.get('/create', createcostume_controller.costume_create_Page);
 router.get('/update', costumeupdate_controller.costume_update_Page); 
 /* GET create costume page */ 
 router.get('/delete', costume_controllers.costume_delete_Page); 
-module.exports = router; 
+module.exports = router;
