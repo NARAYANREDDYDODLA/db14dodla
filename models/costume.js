7b640e2fde74e81f675d733b36be808bca0e233f
NARAYANREDDYDODLA <narayanreddydnr@gmail.com>
2021-12-01 23:19:56 -0600

    updated

--- models/costume.js
+++ models/costume.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const costumeSchema = mongoose.Schema({ 
  costume_type: String, 
- size: String, 
- cost: Number 
+ size: {type: String, maxlength: 5}, 
+ cost:{ type: Number, min: 5, max: 35 }
 },{
     writeConcern: {
       w: 'majority',
